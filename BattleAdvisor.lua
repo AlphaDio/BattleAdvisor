2d9164e94fe38e2a13356cb80b01f730dd8f4688
Alpha Diop <diop.alpha@gmail.com>
2012-04-15 11:04:16 -0400

    trying to put the function variable.

--- BattleAdvisor.lua
+++ BattleAdvisor.lua
@@ -60,7 +60,8 @@ function StartFrames()
     AB_Button:SetWidth(50)
     AB_Button:SetHeight(35)
     AB_Button:SetText("AB")
-    AB_Button:SetCallback("OnClick", function(widget) print("Button Clicked!") end)
+    AB_Button:SetCallback("OnClick", BG_ButtonClick)
+    -- function(widget) print("Button Clicked!") end)
     AB_Button.BG = AB
     BA_MainFrame:AddChild(AB_Button)
 end
